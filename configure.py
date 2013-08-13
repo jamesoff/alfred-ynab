a7703920a25a1f4717b6367969be16a6c0fd2421
James Seward <james-github@jamesoff.net>
2013-08-13 17:57:55 +0100

    Add temporary workaround for file actions not working

--- configure.py
+++ configure.py
@@ -1,6 +1,13 @@
 import alp
 
 path = alp.args()[0]
+if path == "auto":
+    path = ""
+
 s = alp.Settings()
 s.set(**{"budget_path": path})
-print "YNAB budget path set!"
+
+if path == "":
+    print "YNAB budget path set to automatic"
+else:
+    print "YNAB budget path set to %s" % path
