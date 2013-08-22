4f00b3131e0adfc6f9a49035c5ab477c94fb12ee
James Seward <james-github@jamesoff.net>
2013-08-22 17:27:52 +0100

    Hopefully fix case where single apostrophes can break the configuration script being called

--- configure.py
+++ configure.py
@@ -1,13 +1,16 @@
 import alp
 
-path = alp.args()[0]
-if path == "auto":
-    path = ""
+try:
+	path = alp.args()[0]
+	if path == "auto":
+	    path = ""
 
-s = alp.Settings()
-s.set(**{"budget_path": path})
+	s = alp.Settings()
+	s.set(**{"budget_path": path})
 
-if path == "":
-    print "YNAB budget path set to automatic"
-else:
-    print "YNAB budget path set to %s" % path
+	if path == "":
+	    print "YNAB budget path set to automatic"
+	else:
+	    print "YNAB budget path set to %s" % path
+except Exception, e:
+	alp.log("Oh no, an exception while saving configuration:", e)
